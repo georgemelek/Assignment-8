7f8bb03b8f7ad69106290c097bee40098312377d
George Melek <melekgeorge@outlook.com>
2021-01-15 16:26:49 -0500

    first commit

---
+++ src/components/TableCell.js
@@ -0,0 +1,25 @@
+import React from 'react'
+export default class TableCell extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cellColor: ""
+        };
+    }
+    
+    handleColor = () => {
+        this.setState({ 
+            cellColor: this.props.color 
+        });
+    }
+    
+    render(){
+        return (
+            <td className = 'cell'
+            style={{backgroundColor: this.state.cellColor}} 
+            onClick={this.handleColor}
+            >
+            </td>
+        )
+    }
+}
